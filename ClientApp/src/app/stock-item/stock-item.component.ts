5251216a3dd3f5ecfc7e55e4ce14e483a9562005
pgenfer <pgenfer@gmail.com>
2018-08-15 22:03:49 +0200

    add stock news to app

--- ClientApp/src/app/stock-item/stock-item.component.ts
+++ ClientApp/src/app/stock-item/stock-item.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { StockService, Stock } from '../providers/stock.service';
 import { ActivatedRoute } from '@angular/router';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-stock-item',
@@ -9,7 +10,10 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class StockItemComponent implements OnInit {
 
+  public stockNews: StockNews[] = [];
+
   constructor(
+    private readonly _http: HttpClient,
     private readonly _stockService: StockService,
     private _route: ActivatedRoute) {
       this._route.params.subscribe(
@@ -21,8 +25,10 @@ export class StockItemComponent implements OnInit {
 
   public symbol: string;
   public stockItem: Stock;
+  public loadingNews: boolean = true;
 
   ngOnInit() {
+    this.loadingNews = true;
     this._stockService.getStockForSymbol(this.symbol).subscribe(
       stock => {
         this.stockItem = stock;
@@ -32,6 +38,54 @@ export class StockItemComponent implements OnInit {
             x.absoluteOverallChangeInPercent = Math.abs(overallChange);
             x.overallChangeIsPositive = overallChange >= 0;
           });
+        // get news for this stock
+        this._http.get<any[]>(
+          `https://api.newsriver.io/v2/search?query=text%3A((%22${this.stockItem.name}%22%20AND%20stocks)%20OR%20(%22${this.stockItem.name}%22%20AND%20aktie)%20OR%20%22${this.stockItem.name}%22)%20AND%20language%3A(en%20OR%20de)`,
+          //`https://api.newsriver.io/v2/search?query=text%3A%22${this.stockItem.name}%22%20AND%20text%3A'stock'&sortBy=discoverDate&sortOrder=DESC&limit=15`,
+          {
+            headers:{
+              'Authorization':'sBBqsGXiYgF0Db5OV5tAw4hBQbXooMFUzferbKVWkVouNmZbo0W7xpcqovxByq98n2pHZrSf1gT2PUujH1YaQA'
+            }
+          }).subscribe(result => {
+            this.loadingNews = false;
+            this.stockNews = [];
+            result.forEach(entry => {
+              const hostName = entry.hasOwnProperty('website') ? entry.website.hostName : '';
+              const elements = <any[]>(entry.elements);
+              let imageUrl = '';
+              let hasImage = false;
+              if(elements.length > 0){
+                if(elements[0].type == 'Image'){
+                  imageUrl = elements[0].url;
+                  hasImage = true;
+                }
+              }
+
+              this.stockNews.push(
+                new StockNews(
+                  entry.title,
+                  entry.discoverDate,
+                  entry.url,
+                  imageUrl,
+                  hostName,
+                  hasImage
+                )
+              )
+            });
+          }
+        );
       });
+    }
   }
-}
+
+  class StockNews{
+    constructor(
+      public readonly title:string,
+      public readonly date:Date,
+      public readonly url:string,
+      public readonly image:string,
+      public readonly hostName:string,
+      public readonly hasImage: boolean ){
+        console.log('has image ' + hasImage);
+      }
+  }
